0cf36948165ac515dda419cbb9dbcaf0e7b3b49f
Etore-techno <cedric.etore@gmail.com>
2025-09-03 17:34:59 +0200

    Update data_1.js

--- 5eme/sequence 1/data/script/data_1.js
+++ 5eme/sequence 1/data/script/data_1.js
@@ -3,7 +3,7 @@ var imagefond = 'data/images/Sans titre 2.jpeg'
 var couleur = '#0f1bc4'
 
 var questions = [ 
-  /*
+
     {
       question: "Qui suis-je ?<br/> Un baron allemand m'a fabriqué entièrement en bois.",
       answers: ["1", "3", "4", "5", "12", "16", "20", "22", "23", "27", "30", "31", "32", "33", "34", "35", "38", "39", "40", "44", "45"],
@@ -109,7 +109,7 @@ var questions = [
       photo2: "carte12.jpg"
     },
 
-      */
+
     {
       question: "Le rôle d'un objet technique se nomme sa fonction d'usage.<br/>Quelle est la fonction d'usage principale du moyen de transport de la carte 20 ?",
       answers: ["chauffer un plat", "protéger les pieds", "se déplacer sur la route", "contenir une boisson", "laisser une trace", "produire de la musique", "se déplacer sur des rails"],
